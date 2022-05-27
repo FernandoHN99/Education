0e4c5e36592c82b45dd950022f590c8483bd5cf1
Fernando <fernandohneto@gmail.com>
2022-05-27 14:16:20 -0300

    Classes sobre Classes abstratas e priemira tentativa Trab Estrtutura

---
+++ 3_ANO_MAUA/EstruturaDeDados/ListaLigadas/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    //              1    2  3  4  5    6    7   8
+    int vetNext = {NULL, 3, 0, 7, 2,  NULL, 5, NULL};
+    int vetKey  = {NULL, 4, 1, x1, 16, NULL, 9, NULL};
+    int vetPrev = {NULL, 5, 2, 0, 7,  NULL, 4, NULL};
+
+    //              1  2  3  4   5   6   7  8
+    int vetNext = {6,  3, 0, 7,  2,  8,  5, 4};
+    int vetKey  = {x4, 4, 1, x1, 16, x3, 9, x2};
+    int vetPrev = {0,  5, 2, 8,  7,  1,  4, 6};
+}
+
+void addElement(int vet[],int element){
+
+}
+
+void addElement(int vet[],int element){
+
+}
